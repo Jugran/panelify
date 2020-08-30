75a93dd80182d60ad56f13ed6f4da3bfb20ea19b
Samarth Jugran <jugransamarth@gmail.com>
2020-08-30 16:25:08 +0530

    add demo screenshots

--- panelify.py
+++ panelify.py
@@ -8,6 +8,9 @@ import sys
 # TODO: black colored panels not detecting
 # since detecting from smaller res, the detecion should be less forgiving (add erosion/dialation)
 
+# FEATURE: add extra margin in image for inline panels
+#           where there is less or no padding at edges
+
 # Findings -
 # scaling is a factor 
 # 		lower image redude noise but can 
@@ -71,8 +74,15 @@ def panels(image):
 	img = np.copy(image)
 	imgr = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+	#ret, imgr = cv2.threshold(imgr, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 	ret, imgr = cv2.threshold(imgr, 240, 255, cv2.THRESH_BINARY_INV)
+
+	# morphological tranformation
+	kernel = np.ones([3,3], dtype=np.int8)
+	imgr = cv2.morphologyEx(imgr, cv2.MORPH_CLOSE, kernel)
+	
 	contours, heir = cv2.findContours(imgr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 	rects = []
 
 	for cntr in contours:
@@ -91,49 +101,77 @@ def panels(image):
 
 	return rects
 
-def readImage(filename):
+def readImage(filename, scale=1):
 	image = cv2.imread(filename, 1)
 	if scale == 1:
 		return image
-		
+
 	s_img = cv2.resize(image, None, fx = scale, fy = scale, interpolation=cv2.INTER_AREA)
 	return s_img
+
+def saveComics(filename, comics):
+
+	annotation = []
+	for comic, rects in comics:
+		boxes = []
+		# rect x, y, w, h
+		for rect in rects:
+			x, y, w, h = rect
+			box = [x, y, x + w, y + h, 1] # 1 for panel
+			boxes.append(box)
+
+		line = f"{comic} {' '.join(','.join(str(x) for x in b) for b in boxes)}"
+		annotation.append(line + '\n')		
+	
+	with open(filename, 'w') as file:
+		file.writelines(annotation)
+
 
 
 if __name__ == '__main__':
 
 	width, height = 800, 1000
-	scale = 0.1
+	scale = 0.25
 	upscale = 1/scale
 
 	if len(sys.argv) >= 3:
-		filename = sys.argv[1].strip()
+		print('batch mode')
+		filepath = sys.argv[1].strip()
 		start = int(sys.argv[2])
-		num = int(sys.argv[3])
+		num = start + int(sys.argv[3]) -1
+		
+		showrects = bool(sys.argv[4])
 
 		comics = []
 
 		for i in range(start, num):
-			file = filename.replace('+', f'{i:03}')
-			img = readImage(file)
+			filename = filepath.replace('+', f'{i:03}')
+
+			img = readImage(filename, scale)
 
 			rects = panels(img)
 
 			rects = [[round(i*upscale) for i in t] for t in rects]
 			print(rects)
-			comics.append((file, rects))
+			comics.append((filename, rects))
+			
+			#if showrects :
+			#	showRects(rects, image)
+			
+			print(f'{filename} rect count: {len(rects)}')
 
-			# showRects(rects, image)
-
-			print(f'{file} rect count: {len(rects)}')
 	elif len(sys.argv) >= 2 :
 		filename = sys.argv[1].strip()
-		img = readImage(filename)
+		img = readImage(filename, scale)
 		rects = panels(img)
 		rects = [[round(i*upscale) for i in t] for t in rects]
 		print(f'{filename} rect count: {len(rects)}')
 		comics = [(filename, rects)]
 	else:
 		sys.exit(0)
+	
+	if showrects :
+		showComics(comics)
 
-	showComics(comics)
+	saveComics('panels.txt', comics)
+
